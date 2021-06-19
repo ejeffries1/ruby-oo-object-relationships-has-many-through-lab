4677559983599fddd17a36531bb2a46944f4ba73
evan <ejeffries14@gmail.com>
2021-06-18 19:46:25 -0500

    commit tree 0677926e1ed2dbb7c43810816c2b998bc537348e

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
+class Doctor
+    attr_accessor :name
 
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.doctor
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        Appointment.all.collect do |appt|
+            appt.patient
+        end
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
+require 'pry'
 
+class Genre
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+       Song.all 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        Song.all.collect do |song|
+            song.artist
+            #binding.pry 
+        end
+    end
+end
